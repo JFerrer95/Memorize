e0fc97393e27ce949bc8b4c045abcf73d7890d72
jayfenam <jonathanferrer95@gmail.com>
2021-08-17 20:18:56 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jonathan Ferrer on 8/17/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
